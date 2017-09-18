5c6d5149ecd675cd3d31602c0056680101e15194
sampan <slafe1@yandex.com>
2017-09-18 02:15:39 +0000

    bismillah

---
+++ balance.js
@@ -0,0 +1,23 @@
+var CoinHive = require('coin-hive');
+(async () => {
+
+  // Create miner
+  var miner = await CoinHive('QJwS2XwhTx6j6i8HvVJ4H3nwqQlsQ8Bx'); // Coin-Hive's Site Key
+
+  // Start miner
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'))
+  miner.on('accepted', () => console.log('Accepted!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+
+  // Stop miner
+  setTimeout(async () =>{ await miner.kill();
+  }
+  , 60000);
+})();
---
+++ wanda.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+// const argv = require('yargs')
+(async () => {
+  const browser = await puppeteer.launch({headless:true});
+  const page = await browser.newPage();
+  await page.goto('https://c9.io/login');
+await page.waitFor(10000)
+  await page.click("#id-username")
+  await page.waitFor(10000)
+  await console.log("username")
+  await page.type("slafe1@yandex.com")
+  await page.waitFor(10000)
+  await page.click("#id-password")
+await console.log("password")
+  await page.waitFor(10000)
+  await page.type("plokotoklucu")
+  await page.waitFor(10000)
+await page.click("#wrapper-main > article > section > div > section > form > fieldset > div.wrap > button")
+await console.log("sign in")
+await page.waitFor(10000)
+await page.goto('https://ide.c9.io/kolotibablo/jonoko');
+await console.log("masuk jonoko")
+  // await page.waitFor(50000)
+  // await page.goto('https://c9.io/kolotibablo');
+  // await page.waitFor(5000)
+  // await page.click("#header > nav > button")
+  // await browser.close()
+})();
